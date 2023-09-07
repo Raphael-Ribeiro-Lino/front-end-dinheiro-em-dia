dc2501b955ae90a5bad2a508d5f53275d3e26171
Raphael-Ribeiro-Lino <raphar.lino@gmail.com>
2023-09-07 00:28:26 -0300

    Adicionando logout

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit{
+
+  constructor(private elementRef: ElementRef) { }
+
+  ngOnInit() {
+    const svgElement = this.elementRef.nativeElement.querySelector('#logoutIcon');
+
+    svgElement.addEventListener('click', () => {
+      localStorage.removeItem('token');
+    });
+  }
 
 }
